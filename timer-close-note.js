47d9fc58f47d01316d8d2a3d416f680790ba260e
mquangabc <151515527+mquangabc@users.noreply.github.com>
2025-04-10 14:16:55 +0700

    Update timer-close-note.js

--- timer-close-note.js
+++ timer-close-note.js
@@ -5,3 +5,17 @@ const timer = setInterval(()=>{
         noteOpus.click();
     }
 }, 500)
+
+// listener DOM change 
+   const observer = new MutationObserver( () => {
+        const noteContainer = document.querySelector(".special-instructions-label");
+        noteContainer && (console.log("Element found, clicking now"),
+        noteContainer.click(),
+        observer.disconnect())
+    }
+    );
+    observer.observe(document.body, {
+        childList: !0, // !0 abbreviation for true value
+        subtree: !0
+    })
+}
