f9316b7064d36fde8eabf959d5ef6ffa9dd05809
mquangabc <151515527+mquangabc@users.noreply.github.com>
2025-04-18 09:48:06 +0700

    Create fetch-middleware.js

---
+++ fetch-middleware.js
@@ -0,0 +1,62 @@
+   <script>
+      // Lưu lại hàm fetch gốc
+const originalFetch = window.fetch;
+
+// Ghi đè fetch
+window.fetch = async (...args) => {
+  const [resource, config] = args;
+
+  // 📌 Truy cập thông tin yêu cầu
+  const method = config?.method || 'GET';
+  const headers = config?.headers || {};
+  const body = config?.body;
+
+  // Gọi fetch gốc
+  const response = await originalFetch(resource, config);
+
+  // 📌 Truy cập phản hồi
+  const clonedResponse = response.clone();
+  const responseData = await clonedResponse.text();
+
+  if(resource?.includes('cart/add')){
+    const data =  JSON.parse(responseData);
+     console.log('📥 Phản hồi API:', {
+    url: resource,
+    status: response.status,
+    data:data,
+  });
+    if(data?.items[0]?.handle){
+      const id = data?.items[0]?.id;
+      fetch(`/products/${data?.items[0]?.handle}.js`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    const findVariant = data?.variants.find(i => i.id === id);
+      console.log('Success:', findVariant, id);
+    if(findVariant){
+     
+      window.OpusSetCompareAtPrice(id?.toString(), {
+        rate: window?.Shopify?.currency?.rate,
+        compareAtPrice: findVariant.compare_at_price,
+      })
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+
+      
+    }
+    
+  }
+ 
+
+  return response;
+};
+    </script>
